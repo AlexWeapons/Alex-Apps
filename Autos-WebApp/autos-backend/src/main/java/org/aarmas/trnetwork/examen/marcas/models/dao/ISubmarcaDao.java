f7b874199640f855bf2ceab2f458daa193288570
AlexWeapons <alexweapons_xd@hotmail.com>
2023-04-08 14:48:28 -0600

    r

--- Autos-WebApp/autos-backend/src/main/java/org/aarmas/trnetwork/examen/marcas/models/dao/ISubmarcaDao.java
+++
@@ -1,8 +0,0 @@
-package org.aarmas.trnetwork.examen.marcas.models.dao;
-
-import org.aarmas.trnetwork.examen.marcas.models.Submarca;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ISubmarcaDao extends CrudRepository<Submarca, Long>{
-	Submarca findByNombre(String nombre);
-}
